8fe8b7903893343b379fb851742b20dbe1da56f6
Chase Merlin <chasemerlin@gmail.com>
2013-09-05 22:59:36 -0400

    base project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamTaskProto::Application.config.secret_token = '1f906b3a5b3a5f303a94e05d530b7b826aaec6672d069c1fa5b2958a969d41c4ea0fa900787f869823847fad1f0857325ac1d291dc15f679bb15cadeb49dfc2a'
